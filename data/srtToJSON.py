130dadeeac3706c529c5e4f2461119afb8fd2b3e
icicl <37204445+icicl@users.noreply.github.com>
2019-01-16 10:35:16 -0800

    yeetus deletus

--- data/srtToJSON.py
+++
@@ -1,8 +0,0 @@
-import os
-for ff in os.listdir():
-    if ff[-4:]==".srt":
-        with open(ff,errors="ignore") as f:
-            d = f.read()
-        while d[-1] == "\n":d=d[:-1]
-        with open(ff[:-4]+".json","w+") as f:
-            f.write("{\"subs\":["+",\n".join(str([i.replace("\"","\'").replace("\'","[placeholder]") for i in i.split("\n")][:2]+["\\".join([i.replace("\"","\'").replace("\'","[placeholder]") for i in i.split("\n")][2:])]).replace("\'","\"").replace("[placeholder]","\'") for i in d.split("\n\n"))+"\n]}")
